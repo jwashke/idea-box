715bdbc7bd0e770d283e49b7136dfec0aa12e47d
Josh Washke <jwashke@icloud.com>
2016-06-28 08:47:37 -0600

    Post route added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  
+
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: [:index]
+      resources :ideas, only: [:index, :create]
     end
   end
 end
