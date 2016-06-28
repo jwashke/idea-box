f913e340088783db50c9feb70f3418574255751d
Josh Washke <jwashke@icloud.com>
2016-06-28 14:37:59 -0600

    Add thumbs create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :destroy]
+      resources :thumbs, only: [:create]
     end
   end
 end
