292037fc22978741a39c3b6b1c18126fb26760ac
Josh Washke <jwashke@icloud.com>
2016-06-28 09:24:14 -0600

    Wrote request spec for sad path posting idea to api

--- spec/requests/api/v1/ideas_create_spec.rb
+++ spec/requests/api/v1/ideas_create_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "POST /api/v1/ideas" do
-  it "returns 204 no content and creates a new idea in the database" do
+  scenario "with required params" do
+    it "returns 204 no content and creates a new idea in the database" do
 
-    post "/api/v1/ideas", format: :json, title: "Idea Title", body: "Idea Body"
+      post "/api/v1/ideas", format: :json, title: "Idea Title", body: "Idea Body"
 
-    expect(response.status).to eq(204)
+      expect(response.status).to eq(204)
 
-    expect(Idea.count).to eq(1)
-    expect(Idea.first.title).to eq("Idea Title")
-    expect(Idea.first.body).to eq("Idea Body")
+      expect(Idea.count).to eq(1)
+      expect(Idea.first.title).to eq("Idea Title")
+      expect(Idea.first.body).to eq("Idea Body")
+    end
+  end
+
+  scenario "with missing params" do
+    it "responds with 400 and an item is not created" do
+      post "/api/v1/ideas", format: :json, title: "Idea Title", body: "Idea Body"
+
+      expect(response.status).to eq(400)
+
+      expect(Idea.count).to eq(0)
+    end
   end
 end
