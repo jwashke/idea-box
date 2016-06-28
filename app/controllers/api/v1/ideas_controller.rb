23cf0fe7fa9bcbaa108f323bb6f812a8926ba904
Josh Washke <jwashke@icloud.com>
2016-06-28 09:08:06 -0600

    Request spec passes

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,4 +2,16 @@ class Api::V1::IdeasController < Api::V1::ApiController
   def index
     @ideas = Idea.order("created_at DESC")
   end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.save
+    render status: 204
+  end
+
+  private
+
+  def idea_params
+    params.permit(:title, :body)
+  end
 end
