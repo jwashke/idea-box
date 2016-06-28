94c680b1c1f54dc7a51b905cec22c8a59b5c581b
Josh Washke <jwashke@icloud.com>
2016-06-28 14:28:08 -0600

    Add quality cycle methods to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,14 @@ class Idea < ActiveRecord::Base
   validates :title, presence: true
 
   enum quality: %w(swill plausible genius)
+
+  def cycle_quality_up
+    self.plausible! if self.swill?
+    self.genius! if self.plausible?
+  end
+
+  def cycle_quality_down
+    self.plausible! if self.genius?
+    self.swill! if self.plausible?
+  end
 end
