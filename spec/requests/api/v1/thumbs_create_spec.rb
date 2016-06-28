434b8a22e00dd1d8e2217929eab39014bd6cf6d5
Josh Washke <jwashke@icloud.com>
2016-06-28 14:37:20 -0600

    Wrote request spec for thumbs up request

---
+++ spec/requests/api/v1/thumbs_create_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "POST /api/v1/thumbs/" do
+  it "cycles the quality of the idea with given id up" do
+    idea = create(:idea)
+
+    post "/api/v1/thumbs", format: :json, id: idea.id
+
+    expect(response.status).to eq(204)
+
+    updated_idea = Idea.last
+    expect(idea.quality).to eq("plausible")
+  end
+end
