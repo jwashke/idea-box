bb39a22d0290e6ec8f7b0e137a1ca33d9274af04
Josh Washke <jwashke@icloud.com>
2016-06-28 14:51:56 -0600

    Thumbs down request spec passes

--- app/controllers/api/v1/thumbs_controller.rb
+++ app/controllers/api/v1/thumbs_controller.rb
@@ -5,4 +5,11 @@ class Api::V1::ThumbsController < Api::V1::ApiController
     @idea.save
     render status: 204
   end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.cycle_quality_down
+    @idea.save
+    render status: 204
+  end
 end
