aa35544a52db5a716458b058f7f28d9724e4f4b1
Josh Washke <jwashke@icloud.com>
2016-06-30 08:46:49 -0600

    Heroku fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+alert('working');
 //= require jquery
 //= require tether
 //= require bootstrap
