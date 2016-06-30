5da4cc82705d2f58df10e35a8209013b50b8af38
Josh Washke <jwashke@icloud.com>
2016-06-29 18:48:14 -0600

    Update seed file with tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-20.times do
-  Idea.create(
-    title: "#{Faker::Hacker.verb} the #{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
-    body: "#{Faker::Hacker.say_something_smart}",
-    quality: rand(0..2)
-  )
+def create_ideas
+  20.times do
+    idea = Idea.create(
+      title: "#{Faker::Hacker.ingverb} the #{Faker::Hacker.adjective} #{Faker::Hacker.noun}",
+      body: "#{Faker::Hacker.say_something_smart}",
+      quality: rand(0..2)
+    )
+    idea.tags = get_tags
+    idea.save
+  end
 end
+
+def get_tags
+  tags_array = []
+  rand(1..4).times do
+    tag = Tag.where(name: "#{Faker::Hacker.abbreviation}").first_or_create
+    tags_array.push(tag)
+  end
+  tags_array
+end
+
+create_ideas
